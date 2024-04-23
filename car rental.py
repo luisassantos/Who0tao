f70b891daf1e64bba3fc2ee90c8c7ba357e288a3
luisa santos <167921277+Who0tao@users.noreply.github.com>
2024-04-23 20:32:30 -0300

    Create car rental.py
    an activity from the python course

---
+++ car rental.py
@@ -0,0 +1,7 @@
+n1 = float(input("how many days did u rent?))
+  days = n1 * 60
+n2 = float(input(" how many Km did u drive?"))
+  Km = n2 * 0.15
+  s = days + Km
+  print("so u must per day is {} and per Km is {:.2f}, the total payable is {}".format(days, Km, s))
+  
